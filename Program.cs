3785c0080e514b930b156546577f6a5e6fe1354b
unknown <AmmWhi48@millardk12.org>
2026-02-23 20:33:25 -0700

    Added Comment and Introduction

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Name: Ammon, 2/23/26, Lab 6: Madlibs
+
+Console.Clear();
+Console.WriteLine(@"Welcome to MadLibs!
+
+Please enter the provided literary devices, and
+you'll get a wacky story out of it!");
+
